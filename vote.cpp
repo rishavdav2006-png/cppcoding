e3dad9c05168df0bf3c07d89f9c93d268f88efb3
Rishav Raj <rishav.dav2006@gmail.com>
2026-08-20 09:22:48 +0530

    Add files via upload

---
+++ vote.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int age; 
+cout << "Apni Age dalein:";
+cin >> age;
+if (age >= 18) {
+    cout << "Aap vote de sakte hain!" << endl;
+}
+else {
+    cout << "Aap abhi vote nhi de sakte." <<endl;
+}
+return 0;
+}
